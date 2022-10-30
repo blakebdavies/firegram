4bf37d8287be19d5d85987e917b18f4e8982a796
Blake <81705490+blakebdavies@users.noreply.github.com>
2022-10-30 13:08:16 +0000

    created modal for clicked images

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -3,15 +3,16 @@ import useFirestore from "../hooks/useFirestore";
 
 import '../index.css';
 
-const ImageGrid = () => {
+const ImageGrid = ({setSelectedImg}) => {
     const { docs } = useFirestore('images');
     console.log(docs);
 
     return (
         <div className="img-grid">
             {docs && docs.map(doc => (
-            <div className="img-wrap" key={doc?.id}>
-                <img src={doc?.url} alt="colorful-image" />
+            <div className="img-wrap" key={doc?.id}
+            onClick={()=>setSelectedImg(doc.url)}>
+                <img src={doc?.url} alt="colorful lifestyle" />
             </div>
         ))}
         </div>
